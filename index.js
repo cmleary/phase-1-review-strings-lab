40653daea0883448a21c956681368e5e263bb42f
Chris Leary <learymchris@gmail.com>
2023-07-31 14:17:23 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = "Welcome to Flatbook, "
+
+welcomeMessage = welcomeMessage + currentUser + "!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+
+ excitedWelcomeMessage = excitedWelcomeMessage + currentUser.toUpperCase() + "!"
+
+let shortGreeting = `Welcome `
+
+shortGreeting = shortGreeting + ${currentUser[0]} + `!`
